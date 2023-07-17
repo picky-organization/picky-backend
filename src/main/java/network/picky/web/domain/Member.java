4755df83c05c885a9e30fa73fbf482d3e135d6d8
JeongMin <skawjdals96@gmail.com>
2023-07-17 22:10:27 +0900

    test commit

--- src/main/java/network/picky/web/domain/Member.java
+++ src/main/java/network/picky/web/domain/Member.java
@@ -1,12 +1,18 @@
 package network.picky.web.domain;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationaType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
 }
