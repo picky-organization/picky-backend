2d5eba121d9fc6e60808f157724b05a4175fabc1
Kim Joon Su <padomay1352@gmail.com>
2023-07-23 13:05:38 +0900

    [test] jwt 테스트 코드 추가

---
+++ src/test/java/network/picky/web/auth/filter/JwtAuthenticationFilterTest.java
@@ -0,0 +1,100 @@
+package network.picky.web.auth.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import network.picky.web.auth.dto.JwtAuthenticationToken;
+import network.picky.web.auth.exception.TokenAuthenticationException;
+import network.picky.web.auth.exception.TokenInvalidException;
+import network.picky.web.auth.exception.TokenParsingException;
+import network.picky.web.auth.token.BearerTokenResolver;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class JwtAuthenticationFilterTest {
+    private final String TOKEN = "the_token";
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    @DisplayName("정상작동시")
+    void doFilterInternal() throws ServletException, IOException {
+        //given
+        ProviderManager providerManager = Mockito.mock();
+        Authentication authentication = new JwtAuthenticationToken(TOKEN);
+        Mockito.when(providerManager.authenticate(Mockito.any())).thenReturn(authentication);
+        BearerTokenResolver bearerTokenResolver = Mockito.mock();
+        Mockito.when(bearerTokenResolver.resolve(Mockito.any())).thenReturn(this.TOKEN);
+
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(providerManager,bearerTokenResolver);
+
+        HttpServletRequest request = Mockito.mock();
+        HttpServletResponse response = Mockito.mock();
+        FilterChain filterChain = Mockito.mock();
+
+        //when
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+        JwtAuthenticationToken auth = (JwtAuthenticationToken)SecurityContextHolder.getContext().getAuthentication();
+
+        //then
+        assertEquals(auth.getToken(), TOKEN);
+    }
+
+    @Test
+    @DisplayName("Token resolver 에서 예외를 던질떄")
+    void doFilterInternalTokenResolverThrow() throws ServletException, IOException {
+        //given
+        ProviderManager providerManager = Mockito.mock();
+        BearerTokenResolver bearerTokenResolver = Mockito.mock();
+            Mockito.doThrow(TokenInvalidException.class).when(bearerTokenResolver).resolve(Mockito.any());
+
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(providerManager, bearerTokenResolver);
+
+        HttpServletRequest request = Mockito.mock();
+        HttpServletResponse response = Mockito.mock();
+        FilterChain filterChain = Mockito.mock();
+
+        //when.then
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+
+        //then
+        Mockito.verify(providerManager, Mockito.never()).authenticate(Mockito.any());
+    }
+
+    @Test
+    @DisplayName("ProviderManager 에서 예외를 던질 때")
+    void doFilterInternalProviderManagerThrow() throws ServletException, IOException {
+        //given
+        ProviderManager providerManager = Mockito.mock();
+        Mockito.when(providerManager.authenticate(Mockito.any())).thenThrow(TokenAuthenticationException.class);
+        BearerTokenResolver bearerTokenResolver = Mockito.mock();
+        Mockito.when(bearerTokenResolver.resolve(Mockito.any())).thenReturn(this.TOKEN);
+
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(providerManager, bearerTokenResolver);
+
+        HttpServletRequest request = Mockito.mock();
+        HttpServletResponse response = Mockito.mock();
+        FilterChain filterChain = Mockito.mock();
+
+        //when
+        jwtAuthenticationFilter.doFilterInternal(request, response, filterChain);
+
+        //then
+        assertEquals(SecurityContextHolder.getContext(), new SecurityContextImpl());
+    }
+}
---
+++ src/test/java/network/picky/web/auth/token/TokenResolverTest.java
@@ -0,0 +1,67 @@
+package network.picky.web.auth.token;
+
+import jakarta.servlet.http.HttpServletRequest;
+import network.picky.web.auth.exception.TokenInvalidException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpHeaders;
+
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class TokenResolverTest {
+    @Mock
+    HttpServletRequest request;
+    BearerTokenResolver bearerTokenResolver;
+
+    @BeforeEach
+    public void setUp(){
+        bearerTokenResolver = new BearerTokenResolver();
+    }
+
+    @Test
+    @DisplayName("Authorization Header가 없는 경우")
+    public void testResolveNotExistsAuthorizationHeader(){
+        //given
+        Mockito.when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn("");
+
+        //then.when
+        assertThrows(TokenInvalidException.class, () ->
+                bearerTokenResolver.resolve(request));
+    }
+    @Test
+    @DisplayName("Authroization Header가 Bearer가 아닌 경우")
+    public void testResolveAuthorizationHeaderNotBearer(){
+        //given
+        String token = "secret";
+        String tokenPrefix = "Token";
+        String header = tokenPrefix+" "+token;
+        Mockito.when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn(header);
+
+        //then.when
+        assertThrows(TokenInvalidException.class, () ->
+                bearerTokenResolver.resolve(request));
+    }
+
+    @Test
+    @DisplayName("헤더사이에 스페이스가 없는 경우")
+    public void testResolveHeaderNoSpace(){
+        //given
+        String token = "secret";
+        String tokenHeader = "Bearer";
+        String header = tokenHeader+token;
+        Mockito.when(request.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn(header);
+
+        //then
+        assertThrows(TokenInvalidException.class, () ->
+                bearerTokenResolver.resolve(request));
+    }
+
+}
