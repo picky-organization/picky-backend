f85a6d1e2af74e2bc613f5816feeadf3ebc1c0fb
Kim Joon Su <padomay1352@gmail.com>
2023-07-29 14:28:36 +0900

    [feat] project controller 기능 완성
    controller 이름 변경

---
+++ src/main/java/network/picky/web/config/InsertData.java
@@ -0,0 +1,39 @@
+package network.picky.web.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import network.picky.web.category.dto.CategorySaveRequestDto;
+import network.picky.web.category.service.CategoryService;
+import network.picky.web.member.domain.Member;
+import network.picky.web.member.enums.Role;
+import network.picky.web.member.repository.MemberRepository;
+import network.picky.web.tech.dto.TechSaveRequestDto;
+import network.picky.web.tech.service.TechService;
+import org.springframework.context.annotation.Configuration;
+@RequiredArgsConstructor
+@Configuration
+public class InsertData {
+    private final CategoryService categoryService;
+    private final TechService techService;
+    private final MemberRepository memberRepository;
+
+    @PostConstruct
+    public void init(){
+        categoryService.create(new CategorySaveRequestDto("category1"));
+        categoryService.create(new CategorySaveRequestDto("category2"));
+        categoryService.create(new CategorySaveRequestDto("category3"));
+
+        techService.create(new TechSaveRequestDto("tech1"));
+        techService.create(new TechSaveRequestDto("tech2"));
+        techService.create(new TechSaveRequestDto("tech3"));
+
+        Member member = Member.builder()
+                .email("test@test.com")
+                .name("test")
+                .picture("https://tistory1.daumcdn.net/tistory/3095648/attach/ad5c70ba90d7493db85c371ffb9d0f89")
+                .oauth2Id("test")
+                .role(Role.USER)
+                .build();
+        memberRepository.save(member);
+    }
+}
