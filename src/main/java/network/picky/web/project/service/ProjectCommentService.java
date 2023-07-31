217a554507e13ac5aa575e201ae5679e1a7d8699
Kim Joon Su <padomay1352@gmail.com>
2023-07-31 13:03:27 +0900

    [feat] project comment service 기능개발
    * readAll : 특정 프로젝트의 전체 comment 읽기(답글은 안읽음), parameter로 자식 comment 읽을 수 있다.
    * create :  memberId와 projectId를 받아 comment 생성, member나 project가 없으면 생성불가, 답글에는 또 답글을 달 수 없음,
    * read : comment id로 읽는다. 못 찾으면 NotFound
    * update : comment id로 수정. 못 찾으면 NotFound
    * delete : comment id로 삭제, 못 찾으면 NotFound, 연관된 자식 comment 삭제

---
+++ src/main/java/network/picky/web/project/service/ProjectCommentService.java
@@ -0,0 +1,87 @@
+package network.picky.web.project.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import network.picky.web.member.domain.Member;
+import network.picky.web.member.exception.MemberNotFoundException;
+import network.picky.web.member.repository.MemberRepository;
+import network.picky.web.project.domain.Project;
+import network.picky.web.project.domain.ProjectComment;
+import network.picky.web.project.dto.ProjectCommentCreateRequestDto;
+import network.picky.web.project.dto.ProjectCommentResponseDto;
+import network.picky.web.project.dto.ProjectCommentUpdateRequestDto;
+import network.picky.web.project.exception.ProjectCommentBadRequestException;
+import network.picky.web.project.exception.ProjectCommentNotFoundException;
+import network.picky.web.project.exception.ProjectNotFoundException;
+import network.picky.web.project.repository.ProjectCommentRepository;
+import network.picky.web.project.repository.ProjectRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ProjectCommentService {
+    private final MemberRepository memberRepository;
+    private final ProjectRepository projectRepository;
+    private final ProjectCommentRepository projectCommentRepository;
+
+    public List<ProjectCommentResponseDto> readAll(Long projectId, Long parentId) throws ProjectNotFoundException, ProjectCommentNotFoundException {
+        Project project = projectRepository.findById(projectId).orElseThrow(ProjectNotFoundException::new);
+
+        List<ProjectComment> projectComments;
+        if (parentId == null) {
+            projectComments = projectCommentRepository.findAllByProjectAndParentIsNull(project);
+        } else {
+            ProjectComment parent = projectCommentRepository.findById(parentId).orElseThrow(() -> new ProjectCommentNotFoundException("부모 댓글을 찾을 수 없습니다."));
+            projectComments = projectCommentRepository.findAllByParent(parent);
+        }
+
+        return projectComments.stream()
+                .map(projectComment -> new ProjectCommentResponseDto(projectComment, projectCommentRepository.countByParent(projectComment))).toList();
+    }
+
+    public ProjectCommentResponseDto create(Long projectId, Long memberId, ProjectCommentCreateRequestDto projectCommentCreateRequestDto) throws MemberNotFoundException, ProjectNotFoundException, ProjectCommentBadRequestException {
+        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+        Project project = projectRepository.findById(projectId).orElseThrow(ProjectNotFoundException::new);
+        ProjectComment parent = null;
+        if (projectCommentCreateRequestDto.getParentId() != null) {
+            parent = projectCommentRepository.findById(projectCommentCreateRequestDto.getParentId()).orElseThrow(ProjectCommentNotFoundException::new);
+            if (parent.getParent() != null) {
+                throw new ProjectCommentBadRequestException("답글에는 또 답글을 달 수 없습니다.");
+            }
+        }
+
+        ProjectComment projectComment = projectCommentCreateRequestDto.toEntity(member, project, parent);
+
+        ProjectComment savedComment = projectCommentRepository.save(projectComment);
+        savedComment.getProject().increaseCommentCount();
+        return new ProjectCommentResponseDto(savedComment);
+    }
+
+    public ProjectCommentResponseDto read(Long id) throws ProjectCommentNotFoundException {
+        ProjectComment projectComment = projectCommentRepository.findById(id).orElseThrow(ProjectCommentNotFoundException::new);
+        int childSize = projectCommentRepository.countByParent(projectComment);
+        return new ProjectCommentResponseDto(projectComment, childSize);
+    }
+
+    public ProjectCommentResponseDto update(Long id, ProjectCommentUpdateRequestDto projectCommentUpdateRequestDto) throws ProjectCommentNotFoundException {
+        ProjectComment projectComment = projectCommentRepository.findById(id).orElseThrow(ProjectCommentNotFoundException::new);
+        projectComment.update(projectCommentUpdateRequestDto);
+        ProjectComment updatedProjectComment = projectCommentRepository.save(projectComment);
+        int childCount = projectCommentRepository.countByParent(projectComment);
+        return new ProjectCommentResponseDto(updatedProjectComment, childCount);
+    }
+
+    public void delete(Long id) throws ProjectCommentNotFoundException {
+        ProjectComment projectComment = projectCommentRepository.findById(id).orElseThrow(ProjectCommentNotFoundException::new);
+        int childCount = projectCommentRepository.countByParent(projectComment);
+        if (childCount > 0) {
+            projectCommentRepository.deleteAllByParent(projectComment);
+            projectComment.getProject().decreaseCommentCount(childCount);
+        }
+        projectComment.getProject().decreaseCommentCount();
+        projectCommentRepository.delete(projectComment);
+    }
+}
