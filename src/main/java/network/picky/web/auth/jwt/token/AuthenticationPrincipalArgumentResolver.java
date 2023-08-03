275633e34487d9f887d33d4b2e50a8664cb1942c
JeongMin <skawjdals96@gmail.com>
2023-08-04 00:33:40 +0900

    Feature: 헤더에 들어간 토큰 검증

--- src/main/java/network/picky/web/auth/jwt/token/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/network/picky/web/auth/jwt/token/AuthenticationPrincipalArgumentResolver.java
@@ -2,12 +2,15 @@ package network.picky.web.auth.jwt.token;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import network.picky.web.auth.exception.TokenInvalidException;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Component
@@ -24,7 +27,10 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
-        String accessToken = AuthorizationExtractor.extract(request);
+        String accessToken = AuthorizationExtractor.extract(Objects.requireNonNull(request));
+        if(!jwtTokenProvider.validToken(accessToken)){
+            throw new TokenInvalidException();
+        }
         return jwtTokenProvider.getParseClaims(accessToken);
     }
 }
