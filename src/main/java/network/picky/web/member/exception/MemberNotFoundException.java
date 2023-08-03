c6c1300902c6970a2c6351ceb4deadd8b02494ec
JeongMin <skawjdals96@gmail.com>
2023-08-03 23:53:11 +0900

    Feature: accessToken을 확인하기 위해 log 추가

--- src/main/java/network/picky/web/member/exception/MemberNotFoundException.java
+++ src/main/java/network/picky/web/member/exception/MemberNotFoundException.java
@@ -5,8 +5,11 @@ import network.picky.web.common.exception.NotFoundException;
 
 @Getter
 public class MemberNotFoundException extends NotFoundException {
+
     private static final String message = "멤버가 존재하지 않습니다.";
+
     public MemberNotFoundException() {
         super(message);
     }
+
 }
