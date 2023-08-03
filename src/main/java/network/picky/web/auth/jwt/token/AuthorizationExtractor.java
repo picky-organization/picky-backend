d0b79cbaf0e2b84dd2f469abbded2586b1f6731b
JeongMin <skawjdals96@gmail.com>
2023-08-04 00:33:40 +0900

    Feature: 헤더에 들어간 토큰 검증

--- src/main/java/network/picky/web/auth/jwt/token/AuthorizationExtractor.java
+++ src/main/java/network/picky/web/auth/jwt/token/AuthorizationExtractor.java
@@ -2,7 +2,7 @@ package network.picky.web.auth.jwt.token;
 
 import jakarta.servlet.http.HttpServletRequest;
 import network.picky.web.auth.exception.InvalidTokenException;
-import network.picky.web.auth.exception.NotFoundTokenException;
+import network.picky.web.auth.exception.TokenNotProvidedException;
 import org.springframework.http.HttpHeaders;
 
 import java.util.Objects;
@@ -14,7 +14,7 @@ public class AuthorizationExtractor {
     public static String extract(final HttpServletRequest request) {
         String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (Objects.isNull(authorizationHeader)) {
-            throw new NotFoundTokenException();
+            throw new TokenNotProvidedException();
         }
 
         validateAuthorizationFormat(authorizationHeader);
