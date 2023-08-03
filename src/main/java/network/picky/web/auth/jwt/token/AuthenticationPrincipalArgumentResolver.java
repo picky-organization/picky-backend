66fb912895641e4ca7d747653d2737b73de49caf
JeongMin <skawjdals96@gmail.com>
2023-08-03 23:58:21 +0900

    Feature: 로그인을 확인하는 커스텀 애노테이션 생성

---
+++ src/main/java/network/picky/web/auth/jwt/token/AuthenticationPrincipalArgumentResolver.java
@@ -0,0 +1,30 @@
+package network.picky.web.auth.jwt.token;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Login.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        String accessToken = AuthorizationExtractor.extract(request);
+        return jwtTokenProvider.getParseClaims(accessToken);
+    }
+}
