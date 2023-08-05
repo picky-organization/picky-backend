878b4f57afd29405c5ae97d0e94e4e80893fe869
JeongMin <skawjdals96@gmail.com>
2023-08-05 15:25:13 +0900

    Refactor: JwtTokenProvider 위치 수정

--- src/main/java/network/picky/web/auth/dto/AuthUser.java
+++ src/main/java/network/picky/web/auth/dto/AuthUser.java
@@ -1,9 +1,11 @@
 package network.picky.web.auth.dto;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import network.picky.web.member.enums.Role;
 
 @Getter
+@EqualsAndHashCode
 public class AuthUser {
     private final Long id;
     private final Role role;
